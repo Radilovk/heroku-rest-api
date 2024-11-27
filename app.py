0dd253944beaf2884b4646afe12563fa011f7578
Radilovk <radilov.k@gmail.com>
2024-11-27 12:20:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def get_responses():
     return jsonify(rows)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Heroku's PORT or default to 5000
+    app.run(host="0.0.0.0", port=port)
